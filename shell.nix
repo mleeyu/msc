f8f45418df147d298b9b3eab496fc24024722520
mleeyu <mateusleeyu@gmail.com>
2026-07-10 17:25:42 -0300

    first commit

---
+++ shell.nix
@@ -0,0 +1,47 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-26.05") {}, ...}:
+
+let
+  rextendr_0_5_0 = pkgs.rPackages.buildRPackage rec {
+    name = "rextendr-${version}";
+    pname = "rextendr";
+    version = "0.5.0";
+
+    src = pkgs.fetchurl {
+      url = "https://cran.r-project.org/src/contrib/rextendr_${version}.tar.gz";
+      hash = "sha256-RfZfx2bisiiG8uLo+ftZs82jx68Dh44VyD3+zDA6FKs=";
+    };
+
+    propagatedBuildInputs = with pkgs.rPackages; [
+      brio
+      cli
+      desc
+      dplyr
+      glue
+      jsonlite
+      lifecycle
+      pkgbuild
+      processx
+      rlang
+      rprojroot
+      stringi
+      vctrs
+      withr
+    ];
+  };
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    cmake
+    cargo
+    rustc
+
+    (rWrapper.override {
+      packages = with rPackages; [
+        languageserver
+        devtools
+        usethis
+      ] ++ [
+        rextendr_0_5_0
+      ];
+    })
+  ];
+}
